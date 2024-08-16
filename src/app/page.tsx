fa76dbd2664973ee16c483c1fbca68b0be99c879
Matthew Goluba <mkgoluba@outlook.com>
2024-08-16 03:10:57 -0500

    update prettier usages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-'use client'
-import React from 'react'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
-import AuthPageContent from './auth/page'
+'use client';
+import React from 'react';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import AuthPageContent from './auth/page';
 
 export default function Page() {
   return (
     <AppRouterCacheProvider>
       <AuthPageContent />
     </AppRouterCacheProvider>
-  )
+  );
 }
