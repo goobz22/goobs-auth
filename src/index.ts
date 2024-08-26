52abbefef91187f6f601d1d4538d5e5b664da05c
Matthew Goluba <mkgoluba@outlook.com>
2024-08-25 22:53:37 -0500

    cleaned up this is flawed but lets get the entire repo working before we worry too much about it

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 // Export the main authentication page component
 export { default as AuthPageContent } from './app/auth/page';
 
-// Export the configuration loader and its types
-export {
-  default as loadAuthConfig,
-  type AuthConfig,
-  type AuthStep,
-} from './actions/server/auth/configLoader';
-
 // Export the auth utility function
-export { default as authUtility } from './actions/server/auth/authUtility';
-
-// Export the custom middleware
-export { authMiddleware } from './actions/server/auth/customMiddleware';
+export { default as authUtility } from './actions/server/auth/utils/genericDatabaseRecord';
